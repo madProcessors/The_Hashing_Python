73ec291212d914579309b353e94add4c37d699ca
bryancalisto@yahoo.es <bryancalisto@yahoo.es>
2020-09-20 11:58:20 -0500

    refactor: added import of hashlib instead of own-implemented sha256

--- backend/The_Hashing_Python/app/views.py
+++ backend/The_Hashing_Python/app/views.py
@@ -5,8 +5,8 @@ from rest_framework.response import Response
 from .serializers import UserSerializer, HashSerializer, \
 SessionUserSerializer, TextSerializer
 from .models import User, Hash, SessionUser
-from .SHA256.sha256 import SHA256
 from django.contrib.auth import get_user_model
+from hashlib import sha256 
 
 class UserView(viewsets.ModelViewSet):
     serializer_class = UserSerializer
@@ -21,19 +21,23 @@ class SessionUserView(viewsets.ModelViewSet):
     queryset = SessionUser.objects.all()
 
 class HashCreationView(APIView):
-    def get(self, request):
-        return Response('HOLAA', status=status.HTTP_200_OK)
-
     def post(self, request, format=None):
         serializer = TextSerializer(data=request.data)
         if serializer.is_valid():
             text = serializer.data['text']
-            theHash = SHA256.hash(text)
-            # Save hash to DB
-            #User.objects.create(email_adress='test@test.com', password='123')
-            #userModel = get_user_model()
-            #user = userModel.objects.get(id=2)
+            # Create hash 
+            theHash = sha256(text.encode())
+            theHash = theHash.hexdigest() 
             user = User.objects.get(user_id=1)
+            # Save hash to DB
             Hash.objects.create(hash=theHash, user_id=user)
             return Response({'hash': theHash}, status=status.HTTP_201_CREATED)
         return Response('ERROR', status=status.HTTP_400_BAD_REQUEST)
+
+# @api_view(['GET'])
+# def auth(request):
+#     user = get_user_model()
+#     if user:
+#         return Response({'auth':1}, status=status.HTTP_200_OK)
+#     else:
+#         return Response({'auth':0}, status=status.HTTP_200_OK)
